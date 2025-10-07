92c56b3c1833e37d6dcf19c68b0ebf7dc3e912be
tahir <tahir@DESKTOP-RHM7SNO>
2025-10-08 00:49:10 +0300

    Add project files.

---
+++ MyAppQuiz/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace MyAppQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelSidebar = new System.Windows.Forms.Panel();
+            this.lblIE = new System.Windows.Forms.Label();
+            this.lblKAU = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblForgot = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.grpComponents = new System.Windows.Forms.GroupBox();
+            this.btnCheckbox = new System.Windows.Forms.Button();
+            this.btnRadio = new System.Windows.Forms.Button();
+            this.panelSidebar.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            this.grpComponents.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(140)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(26, 300);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(260, 42);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // panelSidebar
+            // 
+            this.panelSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(76)))), ((int)(((byte)(130)))));
+            this.panelSidebar.Controls.Add(this.btnExit);
+            this.panelSidebar.Controls.Add(this.lblForgot);
+            this.panelSidebar.Controls.Add(this.textBox2);
+            this.panelSidebar.Controls.Add(this.lblKAU);
+            this.panelSidebar.Controls.Add(this.lblIE);
+            this.panelSidebar.Controls.Add(this.btnLogin);
+            this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSidebar.Location = new System.Drawing.Point(0, 0);
+            this.panelSidebar.Name = "panelSidebar";
+            this.panelSidebar.Size = new System.Drawing.Size(320, 581);
+            this.panelSidebar.TabIndex = 2;
+            // 
+            // lblIE
+            // 
+            this.lblIE.AutoSize = true;
+            this.lblIE.Font = new System.Drawing.Font("Segoe UI", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIE.ForeColor = System.Drawing.Color.White;
+            this.lblIE.Location = new System.Drawing.Point(26, 22);
+            this.lblIE.Name = "lblIE";
+            this.lblIE.Size = new System.Drawing.Size(75, 71);
+            this.lblIE.TabIndex = 0;
+            this.lblIE.Text = "IE";
+            this.lblIE.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblKAU
+            // 
+            this.lblKAU.AutoSize = true;
+            this.lblKAU.Font = new System.Drawing.Font("Segoe UI", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKAU.ForeColor = System.Drawing.Color.White;
+            this.lblKAU.Location = new System.Drawing.Point(26, 79);
+            this.lblKAU.Name = "lblKAU";
+            this.lblKAU.Size = new System.Drawing.Size(138, 71);
+            this.lblKAU.TabIndex = 1;
+            this.lblKAU.Text = "KAU";
+            this.lblKAU.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(76)))), ((int)(((byte)(130)))));
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.ForeColor = System.Drawing.Color.White;
+            this.lblUser.Location = new System.Drawing.Point(26, 150);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(59, 13);
+            this.lblUser.TabIndex = 3;
+            this.lblUser.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(76)))), ((int)(((byte)(130)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(26, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // txtPass
+            // 
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPass.Location = new System.Drawing.Point(26, 245);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(260, 20);
+            this.txtPass.TabIndex = 5;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(26, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // lblForgot
+            // 
+            this.lblForgot.BackColor = System.Drawing.Color.Transparent;
+            this.lblForgot.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblForgot.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForgot.ForeColor = System.Drawing.Color.LightGray;
+            this.lblForgot.Location = new System.Drawing.Point(26, 271);
+            this.lblForgot.Name = "lblForgot";
+            this.lblForgot.Size = new System.Drawing.Size(115, 24);
+            this.lblForgot.TabIndex = 6;
+            this.lblForgot.Text = "Forgot Password";
+            this.lblForgot.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblForgot.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.lblForgot.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(140)))));
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.No;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(26, 504);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(260, 42);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.grpComponents);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelRight.Location = new System.Drawing.Point(320, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Padding = new System.Windows.Forms.Padding(20);
+            this.panelRight.Size = new System.Drawing.Size(664, 581);
+            this.panelRight.TabIndex = 6;
+            // 
+            // grpComponents
+            // 
+            this.grpComponents.Controls.Add(this.btnRadio);
+            this.grpComponents.Controls.Add(this.btnCheckbox);
+            this.grpComponents.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpComponents.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.grpComponents.Location = new System.Drawing.Point(40, 60);
+            this.grpComponents.Name = "grpComponents";
+            this.grpComponents.Size = new System.Drawing.Size(600, 420);
+            this.grpComponents.TabIndex = 0;
+            this.grpComponents.TabStop = false;
+            this.grpComponents.Text = "Components";
+            // 
+            // btnCheckbox
+            // 
+            this.btnCheckbox.Location = new System.Drawing.Point(40, 110);
+            this.btnCheckbox.Name = "btnCheckbox";
+            this.btnCheckbox.Size = new System.Drawing.Size(520, 60);
+            this.btnCheckbox.TabIndex = 0;
+            this.btnCheckbox.Text = "Checkbox";
+            this.btnCheckbox.UseVisualStyleBackColor = true;
+            this.btnCheckbox.Click += new System.EventHandler(this.btnOpencheckbox_Click);
+            // 
+            // btnRadio
+            // 
+            this.btnRadio.Location = new System.Drawing.Point(40, 190);
+            this.btnRadio.Name = "btnRadio";
+            this.btnRadio.Size = new System.Drawing.Size(520, 60);
+            this.btnRadio.TabIndex = 1;
+            this.btnRadio.Text = "Radio";
+            this.btnRadio.UseVisualStyleBackColor = true;
+            this.btnRadio.Click += new System.EventHandler(this.btnRadio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(238)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(984, 581);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.panelSidebar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainFrm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelSidebar.ResumeLayout(false);
+            this.panelSidebar.PerformLayout();
+            this.panelRight.ResumeLayout(false);
+            this.grpComponents.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelSidebar;
+        private System.Windows.Forms.Label lblIE;
+        private System.Windows.Forms.Label lblKAU;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button lblForgot;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.GroupBox grpComponents;
+        private System.Windows.Forms.Button btnRadio;
+        private System.Windows.Forms.Button btnCheckbox;
+    }
+}
+
---
+++ MyAppQuiz/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyAppQuiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpencheckbox_Click(object sender, EventArgs e)
+        {
+            FrmCheckbox1 frm = new FrmCheckbox1();
+            frm.ShowDialog();
+
+
+
+
+
+        }
+
+        private void btnRadio_Click(object sender, EventArgs e)
+        {
+            FrmCheckbox1 frm = new FrmCheckbox1();
+            frm.ShowDialog();
+        }
+    }
+}
